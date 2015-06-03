e633d3c72944bb082f03e4a799b73b33f5c1d307
unknown <10071633@LDSLT05.lds.ecomms.local>
2015-06-03 11:00:35 +0100

    Items and Users Java Classes

--- src/dwpbay/Item.java
+++ src/dwpbay/Item.java
@@ -1,5 +1,29 @@
 package dwpbay;
 
+import java.time.LocalDateTime;
+
 public class Item {
+	
+	private int itemID ;
+	private String title ;
+	private String category ;
+	private String description ;
+	private String picture ;
+	private Double price ;
+	private int onSale ;
+	private Double reservePrice ;
+	private LocalDateTime startDateTime ;
+	
+	public Item(int itemID, String title, String category, String description, String picture, Double price, int onSale, Double reservePrice, LocalDateTime startDateTime) {
+		this.itemID = itemID ;
+		this.title = title ;
+		this.category = category ;
+		this.description = description ;
+		this.picture = picture ;
+		this.price = price ;
+		this.onSale = onSale ;
+		this.reservePrice = reservePrice ;
+		this.startDateTime = startDateTime ;
+	}
 
 }
--- src/dwpbay/User.java
+++ src/dwpbay/User.java
@@ -2,4 +2,19 @@ package dwpbay;
 
 public class User {
 
+	private int userID ;
+	private String firstName ;
+	private String lastName ;
+	private String email ;
+	private String address ;
+	private String postcode ;
+	
+	public User(int userID, String firstName, String lastName, String email, String address, String postcode) {
+		this.userID = userID ;
+		this.firstName = firstName ;
+		this.lastName = lastName ;
+		this.email = email ;
+		this.address = address ; 
+		this.postcode = postcode ;
+	}
 }
